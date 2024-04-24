f57eda656d8c2c0f359df8a7a44334e084feb654
IvoJunior0 <shymanjr2019@gmail.com>
2024-04-24 20:41:49 -0300

    icones adicionados a tela de login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,19 +1,27 @@
 import Input from "../ui/components/form/Input";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+
 function Login() {
     return(
         <>
             <div className="bg-gradient-to-r from-white to-green-200 h-screen flex justify-center items-center ">
                 <div className="bg-green-600 p-8 rounded-xl w-full max-w-96 shadow-xl mx-4">
-                    <h1 className="text-center text-white font-logo font-bold text-5xl pb-8">Login</h1>
+                    <h1 className="text-center text-white font-logo font-bold text-5xl pb-8"><FontAwesomeIcon icon={faCircleUser} /> Login</h1>
                     <form action="" className="text-center">
-                        <Input type="email" placeholder="Seu email"/>
-                        <Input type="password" placeholder="Sua senha"/>
+                        <Input type="email" placeholder="Seu email">
+                            <FontAwesomeIcon icon={faEnvelope} />
+                        </Input>
+                        <Input type="password" placeholder="Sua senha">
+                            <FontAwesomeIcon icon={faLock} />
+                        </Input>
                         <div className="flex justify-between text-white pb-7">
-                            <div>
+                            <div className="flex gap-2">
                                 <input type="checkbox" />
-                                <label htmlFor="Remeber me">Lembrar login</label>
+                                <label htmlFor="Remeber me">Lembrar-me</label>
                             </div>
                             <span>Esqueçeu sua senha?</span>
                         </div>
--- src/ui/components/form/Input.jsx
+++ src/ui/components/form/Input.jsx
@@ -1,7 +1,10 @@
-function Input({type, placeholder}) {
+function Input({type, placeholder, children}) {
     return(
-        <div className="pb-7">
-            <input type={type} placeholder={placeholder} className="py-3 px-5 rounded-xl w-full outline-none text-neutral-400"/>
+        <div className="pb-7 relative">
+            <input type={type} placeholder={placeholder} className="pl-12 py-3 px-5 rounded-xl w-full outline-none text-neutral-400"/>
+            <div className="absolute top-3 left-5 text-neutral-400">
+                {children}
+            </div>
         </div>
     );
 }
