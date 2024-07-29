1c64acedd7b6e70721e7c523bfae3bd89971687e
Ivo Junior <shymanjr2019@gmail.com>
2024-07-29 09:27:39 -0300

    botão dropdown de filtros

---
+++ src/ui/components/buttons/FilterButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useState } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+function FilterButton() {
+    const [open, setOpen] = useState(false);
+
+    function handleOpen () {
+        setOpen(!open);
+    };
+
+    return (
+        <div className='my-6'>
+            <button className='bg-gray-500 text-white px-4 py-2 rounded' onClick={handleOpen}>
+                Filtrar por <FontAwesomeIcon icon={faChevronDown} />
+            </button>
+            {open ? <div>Aberto</div> : <div></div>}
+        </div>
+
+    )
+}
+
+export default FilterButton
--- src/ui/components/home/Content.jsx
+++ src/ui/components/home/Content.jsx
@@ -1,7 +1,10 @@
+import FilterButton from "../buttons/FilterButton";
+
 export default function Content() {
     return(
         <div className="px-5 w-full mt-[90px] mb-[24px] py-[24px] h-fit col-end-2 max-[1199px]:col-span-full col-start-2">
             <h1 className="sm:text-3xl text-2xl text-gray-500 font-semibold">Últimas publicações</h1>
+            <FilterButton/>
         </div>
     );
 }
