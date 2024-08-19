e7f07c11162e9c44a2f6f43332f70e5ca4fe9b93
Ivo Junior <shymanjr2019@gmail.com>
2024-08-18 22:04:06 -0300

    ajuste na responsividade das tags

--- src/ui/components/Posts/Tag.jsx
+++ src/ui/components/Posts/Tag.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Tag({ tagName }) {
     return (
-        <div className="flex justify-center items-center py-1.5 px-3.5 bg-slate-300 border-2 border-slate-400 rounded-full max-w-30 w-fit hover:bg-slate-400 hover:text-slate-100">
+        <div className="flex justify-center items-center py-1.5 px-3.5 bg-slate-300 border-2 border-slate-400 rounded-full max-w-30 w-fit text-sm hover:bg-slate-400 hover:text-slate-100">
             <h1>{tagName}</h1>
         </div>
     );
