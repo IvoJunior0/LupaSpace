b8f51b449b5ec1e541f9154e881266c553e1c210
Ivo Junior <shymanjr2019@gmail.com>
2024-08-28 23:12:46 -0300

    layout do componente dos subforums

--- src/ui/components/forum/Subforum.jsx
+++ src/ui/components/forum/Subforum.jsx
@@ -4,6 +4,7 @@ import Loading from "../extras/Loading";
 
 import { doc, getDoc, collection } from "firebase/firestore";
 import { db } from "../../../config/firebase";
+import { Link } from "react-router-dom";
 
 export default function Subforum({ subforumId, communityId }) {
     const [subForumData, setSubForumData] = useState({}); 
@@ -15,7 +16,7 @@ export default function Subforum({ subforumId, communityId }) {
             const subforumSnapshot = await getDoc(subforumRef);
             if (subforumSnapshot.exists()) {
                 setSubForumData(subforumSnapshot.data());
-                setLoading(false);                
+                setLoading(false);
             } else {
                 console.log("O documento não existe");
             }
@@ -30,11 +31,16 @@ export default function Subforum({ subforumId, communityId }) {
         }
     }, [communityId, subforumId]);
 
-    if (loading) return (<Loading/>);
+    if (loading) return <Loading/>;
 
     return (
-        <div className="grid grid-cols-2 grid-rows-1 bg-sky-800">
-            {subForumData.name}
+        <div className="grid grid-cols-2 grid-rows-1 p-6 border-slate-300 border-[3px] border-l-[16px] rounded-lg text-gray-500 transition-all hover:border-l-[32px]">
+            <div>
+                <Link to={`${subforumId}`}>{subForumData.name}</Link>
+            </div>
+            <div>
+                Tópico
+            </div>
         </div>
     );
 }
