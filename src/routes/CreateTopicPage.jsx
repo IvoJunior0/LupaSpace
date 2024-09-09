9a89b476c5829b773625cfd6c1c534f5b771df36
Ivo Junior <shymanjr2019@gmail.com>
2024-09-09 17:36:52 -0300

    layout base da pagina de criar tópicos

--- src/routes/CreateTopicPage.jsx
+++ src/routes/CreateTopicPage.jsx
@@ -4,18 +4,18 @@ export default function CreateTopicPage() {
             <hr className="border-t-2"/>
             <form className="flex flex-col gap-5 text-gray-500">
                 <div>
-                    <h1 className="text-xl">Criar novo tópico</h1>
+                    <h1 className="text-xl font-bold">Criar novo tópico</h1>
                 </div>
                 <div className="">
-                    <label className="block">Título</label>
+                    <label className="block font-semibold">Título</label>
                     <input type="text" placeholder="Título do tópico" className="block w-full p-2.5 bg-slate-100 border-slate-300 border-2 rounded-md focus:outline-none focus:border-slate-400"/>
                 </div>
                 <div className="">
-                    <label className="block">Conteúdo</label>
+                    <label className="block font-semibold">Conteúdo</label>
                     <textarea name="" id="" placeholder="Conteúdo do tópico" className="block resize-none w-full min-h-44 p-2.5 bg-slate-100 border-slate-300 border-2 rounded-md focus:outline-none focus:border-slate-400"/>
                 </div>
-                <div className="">
-                    <button type="submit" className="text-white py-2 px-4 bg-green-600 hover:bg-green-500">Publicar</button>
+                <div className="self-end">
+                    <button type="submit" className="text-white py-2 px-4 rounded bg-green-600 hover:bg-green-500">Publicar</button>
                 </div>
             </form>
         </>
