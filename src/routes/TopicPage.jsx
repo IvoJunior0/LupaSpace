6e872dd263bdca42c733863a2f806dd113879849
Ivo Junior <shymanjr2019@gmail.com>
2024-09-19 21:40:01 -0300

    mostrando informações do tópico na página

--- src/routes/TopicPage.jsx
+++ src/routes/TopicPage.jsx
@@ -10,11 +10,13 @@ export default function TopicPage() {
     const { topicID } = useParams();
     const location = useLocation();
     const [topicData, setTopicData] = useState({});
+    const [authorData, setAuthorData] = useState({});
     const [message, setMessage] = useState("");
     const [loading, setLoading] = useState(true);
     
     useEffect(() => {
         const fetchTopicData = async () => {
+            // Informações do tópico
             const splittedPath = location.pathname.split("/");
             const splicedPath = splittedPath.splice(2,3);
 
@@ -22,19 +24,38 @@ export default function TopicPage() {
 
             const topicRef = doc(db, path, topicID);
             const topicDoc = await getDoc(topicRef);
+            const data = topicDoc.data();
 
-            topicDoc.exists() ? setTopicData(topicDoc.data()) : setMessage("Topico não encontrado")
+            topicDoc.exists() ? setTopicData(topicDoc.data()) : setMessage("Topico não encontrado");
+
+            // Informações do autor
+            const authorRef = doc(db, "Users", data.authorID);
+            const authorSnapshot = await getDoc(authorRef);
+
+            authorSnapshot.exists() ? setAuthorData(authorSnapshot.data()) : null;
+
+            setLoading(false);
         }
         fetchTopicData();
     }, [topicID, location]);
 
+    // Mudando o título da página
+    (topicData && !loading) ? document.title = `${topicData.title}` : document.title = "Carregando...";
+
+    if (loading) return <Loading/>;
+
     console.log(topicData)
 
     return (
-        <>
+        <div className="text-gray-500 p-4">
             <div className="">
+                <h5>Por há</h5>
                 <h1 className="">{topicData.title}</h1>
+                <h2>{topicData.replyCount} respostas</h2>
+                <h2>{topicData.viewCount} visualizações</h2>
+                <hr />
+                <p>{topicData.content}</p>
             </div>
-        </>
+        </div>
     );
 }
