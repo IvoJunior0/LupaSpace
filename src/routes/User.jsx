4937c7fbf9720c18542f869a08d58394b7573279
Ivo Junior <shymanjr2019@gmail.com>
2024-10-23 11:44:08 -0300

    header da pagina de usuario

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -22,31 +22,31 @@ export default function User() {
 
     useEffect(() => {
         const fetchUserData = async () => {
-          const userDoc = await getDoc(doc(db, 'Users', uid));
+            const userDoc = await getDoc(doc(db, 'Users', uid));
             
-          if (userDoc.exists()) {
-            setUserData(userDoc.data());
-          } else {
-            console.log('Usuário não encontrado');
-          }
-          setLoading(false);
+            if (userDoc.exists()) {
+                setUserData(userDoc.data());
+            } else {
+                console.log('Usuário não encontrado');
+            }
+            setLoading(false);
         };
     
         fetchUserData();
     }, [uid]);
 
     if (loading) {
-      return (
-        <div className="h-screen w-screen">
-          <Loading/>
-        </div>
-      );
+        return (
+            <div className="h-screen w-screen">
+                <Loading/>
+            </div>
+        );
     }
 
     if (!userData) {
-      return <div>Usuário não encontrado</div>;
+        return <div>Usuário não encontrado</div>;
     } else {
-      document.title = `${userData.name} no LupaSpace`;
+        document.title = `${userData.name} (@${userData.username}) | LupaSpace`;
     }
 
     return(
@@ -54,16 +54,20 @@ export default function User() {
             <Navbar/>
             <Sidebar/>
             <div className="mt-[90px] mb-[24px] w-full h-fit col-end-2 max-[1199px]:col-span-full col-start-2">
-                  <h2 className='relative z-20'>
-                    {!userData.pfp ? (
-                      <FontAwesomeIcon icon={faCircleUser} className='text-6xl text-green-500 border-4 border-white rounded-full'/>
-                    ) : userData.pfp}
-                  </h2>
-                  <div className="">
-                    <h2>{userData.name}</h2>
-                    <h2>@{userData.username}</h2>
-                  </div>
-                  <button>Seguir +</button>
+                <div className="flex items-center justify-around w-full bg-green-800 p-3">
+                    <h2 className=''>
+                        {!userData.pfp ? (
+                            <FontAwesomeIcon icon={faCircleUser} className='text-7xl text-green-500 border-4 border-white rounded-full'/>
+                        ) : userData.pfp}
+                    </h2>
+                    <div className="text-white">
+                        <h2 className='font-bold'>{userData.name}</h2>
+                        <h2>@{userData.username}</h2>
+                    </div>
+                    <div className="">
+                        <button className='bg-green-500 text-white py-1.5 px-3.5 rounded'>Seguir +</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
